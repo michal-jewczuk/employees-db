531e543470c79072b8a3b06f0356ca6c9d01218a
michal-jewczuk <michal.jewczuk@outlook.com>
2025-11-16 08:38:40 +0100

    Add guards for list employees

--- src/parse.c
+++ src/parse.c
@@ -173,6 +173,10 @@ int add_employee(struct dbheader_t *header, struct employee_t **employees, char 
 }
 
 void list_employees(struct dbheader_t *header, struct employee_t *employees) {
+	if (header == NULL || employees == NULL) {
+		return;
+	}
+
 	for (int i = 0; i < header->count; i++) {
 		printf("[%d] %s,%s,%d\n", i + 1, employees[i].name, employees[i].address, employees[i].hours);
 	}
